b05b7c513d0f6326f7fdde6f8ab7017bdb35cf57
Tom Turner <tom@tect.co.uk>
2013-06-13 16:51:55 +0200

    Added Subs text

--- OSM_API/OSM_API/OSM_Functions_Config.php
+++ OSM_API/OSM_API/OSM_Functions_Config.php
@@ -3,5 +3,8 @@ $OSM_NoDueBadges = "There are no due badges.";
 $OSM_NoCloseBirthdays = "There are no birthdays in the next 4 weeks.";
 $OSM_NoDueSubs = "All the subs are upto date";
 $OSM_NoScoutsLinkedToParent = "There are no Scouts linked to your account.  Please talk to one of your leaders to link your child with your account.";
-
+$OSM_ScoutMovingDateEstimated = "  does not yet have a confirmed moving on date, however, they will be due to move to ";
+$OSM_ScoutMovingDate = " is due to move up to ";
+$OSM_SubsPaid = "'s subs are currently up-to-date.";
+$OSM_SubsDue = " is due in subs for ";
 ?>
