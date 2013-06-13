27b33d6906f55575a87dfd74cdd2c1741c2eaba6
Tom Turner <tom@tect.co.uk>
2013-06-13 16:03:53 +0200

    added OSM_NoScoutsLinkedToParent

--- OSM_API/OSM_API/OSM_Functions_Config.php
+++ OSM_API/OSM_API/OSM_Functions_Config.php
@@ -2,4 +2,6 @@
 $OSM_NoDueBadges = "There are no due badges.";
 $OSM_NoCloseBirthdays = "There are no birthdays in the next 4 weeks.";
 $OSM_NoDueSubs = "All the subs are upto date";
+$OSM_NoScoutsLinkedToParent = "There are no Scouts linked to your account.  Please talk to one of your leaders to link your child with your account.";
+
 ?>
