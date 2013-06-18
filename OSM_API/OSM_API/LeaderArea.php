cb13622d5aca5efd0bad4fa012b1280071385c06
Tom Turner <tom@tect.co.uk>
2013-06-18 12:33:14 +0200

    Added Parent Area Config Section Config Form

--- OSM_API/OSM_API/LeaderArea.php
+++ OSM_API/OSM_API/LeaderArea.php
@@ -143,18 +143,40 @@ function SectionConfigForm()
 		
 		$HTML .= "<form method=\"post\">";
 		$HTML .= '<table>';
-		
+		$HTML .= '<tr><td colspan="2"><h3>Section Details</h3></td></tr>';
 		$HTML .= '<tr><td>Section Name</td>';
 		$HTML .= '<td><input type="text" maxlength="50" required value="{$Section->GetName()}" /></td></tr>';
 		
 		$HTML .= '<tr><td colspan="2"><h3>Section Finances</h3><p>Enter the subs amounts for the various intervals, if you enter 0 then that payment option will not show.</td></tr>';
+		$HTML .= '<tr><td>Enable Subs</td>';
+		$HTML .= '<td><input name="EnableSubs" type="checkbox" /></td></tr>';
 		$HTML .= '<tr><td>Weekly Subs</td>';
 		$HTML .= '<td><input name="Weekly" type="number" step="0.01" value="{$Section->GetWeeklySubs()}" /></td></tr>';
 		$HTML .= '<tr><td>Half Term Subs</td>';
 		$HTML .= '<td><input name="HalfTerm" type="number" step="0.01" value="{$Section->GetWeeklySubs()}" /></td></tr>';
 		$HTML .= '<tr><td>Full Term Subs</td>';
 		$HTML .= '<td><input name="Term" type="number" step="0.01" value="{$Section->GetWeeklySubs()}" /></td></tr>';
+		$HTML .= '<tr><td colspan="2"><h3>Parents Area Config</h3><p>This section configures what the parents in the section see.</p></td></tr>';
+		$HTML .= '<tr><td>Enable view Payments</td>';
+		$HTML .= '<td><input name="EnablePayments" type="checkbox" /></td></tr>';
 		
+		$HTML .= '<tr><td>View Scout Details</td>';
+		$HTML .= '<td><input name="EnableViewDetails" type="checkbox" /></td></tr>';
+		$HTML .= '<tr><td>Edit Scout Details</td>';
+		$HTML .= '<td><input name="EnableEditDetails" type="checkbox" /></td></tr>';
+		$HTML .= '<tr><td>View Moving Date</td>';
+		$HTML .= '<td><input name="EnableMovingDate" type="checkbox" /></td></tr>';
+		
+		$HTML .= '<tr><td>View Events</td>';
+		$HTML .= '<td><input name="EnableViewEvents" type="checkbox" /></td></tr>';
+		
+		$HTML .= '<tr><td>View Event Responses</td>';
+		$HTML .= '<td><input name="EnableEventResponse" type="checkbox" /></td></tr>';
+		$HTML .= '<tr><td>View Program</td>';
+		$HTML .= '<td><input name="EnableViewProgram" type="checkbox" /></td></tr>';
+		
+		$HTML .= '<tr><td>View Events</td>';
+		$HTML .= '<td><input name="EnableViewEvents" type="checkbox" /></td></tr>';
 		$HTML .= '</table>';
 		$HTML .= '<input type="checkbox" name="UpdateOSM" checked /> Update OSM';
 		$HTML .= '<input type="submit" value="Submit Data" />';
